740fc9055ce231ec2c0e51f82bfcc54481ffe7dc
zobad16 <zobad.mahmood@gmail.com>
2020-04-26 16:14:48 +0300

    Adapter Class for login

--- BookStore/Models/LoginAdapter.cs
+++ BookStore/Models/LoginAdapter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//This is an adapter class that uses the IUser interface and create an adapter class through which a differnt class login can be used 
 namespace BookStore.Models
 {
     public class LoginAdapter : IUser
